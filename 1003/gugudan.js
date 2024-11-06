b1dac3b69ac09a8f48adfc7bbc97543e40955ed5
daitdae <junhyung@Junhyungui-MacBook-Pro.local>
2024-11-06 19:04:13 +0900

    24.11.06 last

--- 1003/gugudan.js
+++ 1003/gugudan.js
@@ -1,4 +1,5 @@
 // 화살표 함수
+//dan = 매개변수
 const gugudan1st = (dan) => {
   for (let i=1; i <= 9; i++){
       console.log(`${dan} * ${i} = ${dan * i}`);
@@ -12,6 +13,7 @@ function gugudan2nd(dan){
     }
 }
 
+// ()안에 3,6,9가 인수
 gugudan1st(3); // 3단 출력
 gugudan2nd(6); // 6단 출력
 gugudan2nd(9); // 9단 출력
