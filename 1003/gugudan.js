cf2904ea125accd5c89251a8ff894b7eed0fc8d8
daitdae <junhyung@Junhyungui-MacBook-Pro.local>
2024-11-06 18:24:07 +0900

    24.11.06

--- 1003/gugudan.js
+++ 1003/gugudan.js
@@ -3,14 +3,14 @@ const gugudan1st = (dan) => {
   for (let i=1; i <= 9; i++){
       console.log(`${dan} * ${i} = ${dan * i}`);
   }
-};
+}
 
 // 함수 선언문
 function gugudan2nd(dan){
     for (let i = 1; i <= 9; i++){
         console.log(`${dan} * ${i} = ${dan * i}`);
     }
-};
+}
 
 gugudan1st(3); // 3단 출력
 gugudan2nd(6); // 6단 출력
