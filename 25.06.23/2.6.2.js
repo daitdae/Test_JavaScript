7faff4a4f68d848526b8ed9bcb7d28a7ce2b5b30
JH <ez2gogo@gmail.com>
2025-06-23 17:46:45 +0900

    24.11.20 5.3 p.301

---
+++ 25.06.23/2.6.2.js
@@ -0,0 +1,18 @@
+// 함수 형식
+// 1. function 이름() {실행문}
+// 2. 이름 = function () {실행문}
+// 3. () => {실행문}
+//    () => 반환식
+
+// function a(){
+//     console.log('Hello');
+//     console.log('Function');
+// }
+// a();
+// a();
+// a();
+
+function a(){
+    return 10;
+}
+a();
